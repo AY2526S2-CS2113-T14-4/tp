9393805e1b06041e262c7bc0bae6bf9b6fc04620
SWIFT14\yiche <e1406485@u.nus.edu>
2026-04-01 22:33:03 +0800

    Corrrect checkstyle issues, which are mainly just adding a line :/

--- src/test/java/seedu/duke/category/CategoryManagerTest.java
+++ src/test/java/seedu/duke/category/CategoryManagerTest.java
@@ -2,10 +2,8 @@ package seedu.duke.category;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.io.TempDir;
 
 import java.lang.reflect.Field;
-import java.nio.file.Path;
 
 import static org.junit.jupiter.api.Assertions.*;
 
